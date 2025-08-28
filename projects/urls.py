017f9208304cd22892623877861ca608c631e9cd
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 21:25:10 +0100

    Tasks views added using nested routing

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from projects.views import ProjectListView
 from projects.views import ProjectDetailView
+from tasks.views import TaskListView, TaskDetailView 
 
 
 urlpatterns = [
     path('', ProjectListView.as_view()),
-    path('<int:pk>/', ProjectDetailView.as_view())
+    path('<int:pk>/', ProjectDetailView.as_view()),
+    path('<int:pk>/tasks/', TaskListView.as_view()),
+    path('<int:pk>/tasks/<int:task_pk>/', TaskDetailView.as_view()),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Task
+from projects.models import Project 
+from .serializers.common import TaskSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class TaskListView(ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(parent_project_id=self.kwargs['pk'])
+    
+    def perform_create(self, serializer): 
+        serializer.save(parent_project_id=self.kwargs['pk'])
+
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class TaskDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'task_pk'
+
+    def get_queryset(self):
+        project_pk = self.kwargs['pk']
+        get_object_or_404(Project, pk=project_pk) 
+        return Task.objects.filter(parent_project_id=project_pk)
+    
+    # permission_classes = [IsAuthenticatedOrReadOnly]
