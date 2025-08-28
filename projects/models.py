3a312e8ea455daa27419beae995a630014ca0164
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 16:23:14 +0100

    Projects app & model added

---
+++ projects/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ('in_progress', 'In Progress'),
+        ('review', 'Under Review'),
+        ('completed', 'Completed'),
+        ('on_hold', 'On Hold'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    name = models.CharField(max_length=255)
+    brief = models.TextField(max_length=2000)
+    deadline = models.DateField() 
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default='in_progress') 
+
+    images = ArrayField(models.URLField(max_length=500), size=4, default=list, blank=True, help_text='Upload max 4 image urls')
+
+    team = models.ForeignKey(to='teams.Team', on_delete=models.SET_NULL, null=True, blank=True, related_name='team_projects')
+    owner = models.ForeignKey(to='users.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='owned_projects')
+
+
+    def __str__(self):
+        return self.name
+
--- users/models.py
+++ users/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
     job_title = models.CharField(max_length=150, blank=True, default='')
-    profile_img = models.CharField(max_length=255, blank=True, help_text="Cloudinary public_id")
+    profile_img = models.URLField(max_length=255, blank=True, help_text="Cloudinary image URL")
     team = models.ForeignKey(to='teams.Team', on_delete=models.SET_NULL, null=True, blank=True)
 
