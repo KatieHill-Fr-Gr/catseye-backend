2c9574a6593d928bf360a900b3c67c635e3610ed
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 13:14:28 +0100

    sign-up route successfully added to users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# from .serializers.common import AuthSerializer
+from .models import User
 
-# Create your views here.
+# * Path: /users/sign-up
+
+class SignUpView(APIView):
+
+    def post(self, request):
+        return Response("Hit sign-up view")
+
+
