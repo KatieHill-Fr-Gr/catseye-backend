99ab3961343b0938610eea19afad4fb904f144c1
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 13:37:34 +0100

    User serializer added

---
+++ users/serializers/common.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from ..models import User
+
+class AuthSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'password_confirmation', 'job_title', 'profile_img', 'team']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirmation']:
+            raise serializers.ValidationError({'Password': 'Passwords do not match'})
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirmation')
+        print('Validated:', validated_data)
+        return User.objects.create_user(**validated_data)
