f496fb6aa895d045527b40274a108731c20df1ce
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 15:02:54 +0100

    Token serializer added to Users app

---
+++ users/serializers/token.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class TokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['user'] = {
+            'id': user.id,
+            'username': user.username
+        }
+
+        return token
