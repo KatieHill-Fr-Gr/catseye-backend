da161389afd44e9c43573a872b7a505b51816124
Katie Hill <khill.fr.gr@gmail.com>
2025-08-28 12:52:58 +0100

    Django setup, User app + model, Team app + model

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    job_title = models.CharField(max_length=150, blank=True, default='')
+    profile_img = models.CharField(max_length=255, blank=True, help_text="Cloudinary public_id")
+    team = models.ForeignKey(to='teams.Team', on_delete=models.SET_NULL, null=True, blank=True)
+
