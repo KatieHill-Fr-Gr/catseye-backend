b7d8ce8c844a5679ff1cfe1e1b9310d50e3a6b49
Katie Hill <khill.fr.gr@gmail.com>
2025-09-02 09:55:59 +0100

    TeamSerializer added to PopulatedProjectSerializer to get team name

--- projects/serializers/populated.py
+++ projects/serializers/populated.py
@@ -1,5 +1,7 @@
 from .common import ProjectSerializer
 from users.serializers.common import OwnerSerializer
+from teams.serializers.common import TeamSerializer
 
 class PopulatedProjectSerializer(ProjectSerializer):
     owner = OwnerSerializer()
+    team = TeamSerializer()
